e7f58fc39c56c8573f1d8660dc364e9017f11ed7
Aldi Arief <aldiarief598@gmail.com>
2025-05-19 11:58:07 +0700

    feat(oauth2): implement OAuth2ClientGuard

--- oauth2-resource-server/src/Guard/OAuth2ClientGuard.php
+++ oauth2-resource-server/src/Guard/OAuth2ClientGuard.php
@@ -12,17 +12,19 @@ use HyperfExtension\Auth\Contracts\GuardInterface;
 use HyperfExtension\Auth\Contracts\UserProviderInterface;
 use HyperfExtension\Auth\GuardHelpers;
 use Menumbing\OAuth2\Resource\Contract\AccessTokenProviderInterface;
+use Menumbing\OAuth2\Resource\OAuth2GuardHelpers;
 use Menumbing\OAuth2\Resource\ResourceServerAuthenticator;
+use Menumbing\OAuth2\Resource\Util\RequestExtractor;
+use Menumbing\OAuth2\Resource\ValidatesScopes;
 use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 /**
  * @author  Aldi Arief <aldiarief598@gmail.com>
  */
 class OAuth2ClientGuard implements GuardInterface
 {
-    use GuardHelpers;
-
-    protected AccessTokenProviderInterface $accessTokenProvider;
+    use GuardHelpers, OAuth2GuardHelpers, ValidatesScopes;
 
     public function __construct(
         protected RequestInterface            $request,
@@ -33,15 +35,55 @@ class OAuth2ClientGuard implements GuardInterface
     )
     {
         $this->provider = $provider;
+
+        $this->accessTokenProvider = $this->getAccessTokenProvider(
+            $this->config->get('oauth2-resource-server.access_token.provider', 'stateless')
+        );
     }
 
     public function user(): ?AuthenticatableInterface
     {
-        // TODO: Implement user() method.
+        if (null !== $this->user) {
+            return $this->user;
+        }
+
+        if (RequestExtractor::bearerToken($this->request)) {
+            $user = $this->authenticateWithBearerToken($this->request);
+        } else if ($token = $this->request->cookie($this->config->get('oauth2-server.cookie.name', 'oauth2_token'))) {
+            $request = $this->request->withHeader('Authorization', 'Bearer ' . $token);
+            $user = $this->authenticateWithBearerToken($request);;
+        }
+
+        return $this->user = $user ?? null;
     }
 
     public function validate(array $credentials = []): bool
     {
         throw new BadMethodCallException();
+    }
+
+    protected function authenticateWithBearerToken(ServerRequestInterface $request): ?AuthenticatableInterface
+    {
+        $bearerToken = RequestExtractor::bearerToken($request);
+        $request = $this->authenticator->authenticateRequest($request);
+
+        $this->validateScopes($request);
+
+        if (null === $user = $this->retrieveClient($request->getAttribute('oauth_client_id'), $bearerToken)) {
+            return null;
+        }
+
+        if (null === $this->retrieveAccessToken($request->getAttribute('oauth_access_token_id'), $bearerToken)) {
+            return null;
+        }
+
+        return $user;
+    }
+
+    protected function getAccessTokenProvider(string $name): AccessTokenProviderInterface
+    {
+        return $this->container->get(
+            $this->config->get('oauth2-resource-server.access_token.providers.' . $name),
+        );
     }
 }
