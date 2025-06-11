ad94c693a46e346266ccfd693e1aa3650c766667
Iqbal Maulana <iq.bluejack@gmail.com>
2025-06-11 18:30:10 +0700

    fix(event-stream): fix missing stream tracking in ConsumerManager
    Added logic to track processed streams by setting `$streams[$key]` to true. This ensures accurate management of already-handled streams during iteration.

--- event-stream/src/Consumer/ConsumerManager.php
+++ event-stream/src/Consumer/ConsumerManager.php
@@ -77,6 +77,8 @@ class ConsumerManager
                 continue;
             }
 
+            $streams[$key] = true;
+
             yield $key => [
                 'name'   => $annotation->stream,
                 'driver' => $annotation->driver,
