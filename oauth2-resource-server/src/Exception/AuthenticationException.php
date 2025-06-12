dc5feac1e0d739253ce672142b4f2c7c37c24738
Aldi Arief <aldiarief598@gmail.com>
2025-06-12 16:58:59 +0700

    feat(oauth2): implement HasHttpResponseInterface in OAuth2 Resource Server Exceptions

--- oauth2-resource-server/src/Exception/AuthenticationException.php
+++ oauth2-resource-server/src/Exception/AuthenticationException.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 namespace Menumbing\OAuth2\ResourceServer\Exception;
 
 use Exception;
+use Menumbing\Contract\Exception\HasHttpResponseInterface;
+use Menumbing\Resource\Trait\ExceptionWithPayload;
 
 /**
  * @author  Iqbal Maulana <iq.bluejack@gmail.com>
  */
-class AuthenticationException extends Exception
+class AuthenticationException extends Exception implements HasHttpResponseInterface
 {
+    use ExceptionWithPayload;
+
     public function __construct($message = 'Unauthenticated.', $code = 401)
     {
         parent::__construct($message, $code);
--- oauth2-resource-server/src/Exception/MissingScopeException.php
+++ oauth2-resource-server/src/Exception/MissingScopeException.php
@@ -6,12 +6,16 @@ namespace Menumbing\OAuth2\ResourceServer\Exception;
 
 use Hyperf\Collection\Arr;
 use HyperfExtension\Auth\Exceptions\AuthorizationException;
+use Menumbing\Contract\Exception\HasHttpResponseInterface;
+use Menumbing\Resource\Trait\ExceptionWithPayload;
 
 /**
  * @author  Iqbal Maulana <iq.bluejack@gmail.com>
  */
-class MissingScopeException extends AuthorizationException
+class MissingScopeException extends AuthorizationException implements HasHttpResponseInterface
 {
+    use ExceptionWithPayload;
+
     public readonly array $scopes;
 
     public function __construct($scopes = [], $message = 'Token has invalid/missing scope(s).', $code = 403)
